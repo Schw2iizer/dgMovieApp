1a890e1f71a610db9c96e2eecf153f43b34e7a3b
Jeff Telford <jefftelford@gmail.com>
2019-05-07 23:42:06 -0600

    removed unused code

--- scripts/components/button.component.js
+++
@@ -1,9 +0,0 @@
-var dgMovieApp = angular.module('dgMovieApp')
-
-.component('buttonComponent', {
-  bindings: {
-    text: '@',
-    active: '@',
-  },
-  template: `<button class="years-button" ng-class="{'active': $ctrl.active}">{{$ctrl.text}}</button>`
-})
